b5fb31a7ed40d5bf3ec33e9af8fb66bf7b3ef194
Tayyab Mansoor <110661825+M-Tayyab15@users.noreply.github.com>
2025-01-31 09:53:49 +0500

    Update quotes.php

--- Core Php/Random_Quotes_Generator/quotes.php
+++ Core Php/Random_Quotes_Generator/quotes.php
@@ -1,11 +1,27 @@
 <?php
-$quotes = [
+$muslimQuotes = [
+    "He who has a why to live can bear almost any how. – Friedrich Nietzsche (often attributed to Imam Al-Ghazali)",
+    "Take account of yourselves before you are taken to account. – Umar ibn al-Khattab",
+    "The best of people are those who are most beneficial to others. – Prophet Muhammad (PBUH)",
+    "Acquire knowledge and teach it to the people. – Imam Ali ibn Abi Talib",
+    "Whoever shows you your faults, do not hate them, for they are showing you the way to salvation. – Imam Ali ibn Abi Talib",
+    "Actions are judged by intentions, so each person will get what they intended. – Prophet Muhammad (PBUH)",
+    "Do not grieve, indeed Allah is with us. – Quran 9:40",
+    "The strong person is not the one who is able to overpower others, but the one who controls themselves in a fit of rage. – Prophet Muhammad (PBUH)"
+];
+
+$otherQuotes = [
     "The only way to do great work is to love what you do. – Steve Jobs",
     "The best time to plant a tree was 20 years ago. The second best time is now. – Chinese Proverb",
     "It does not matter how slowly you go as long as you do not stop. – Confucius",
     "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
     "Believe you can and you're halfway there. – Theodore Roosevelt",
     "Do not go where the path may lead, go instead where there is no path and leave a trail. – Ralph Waldo Emerson",
-    "You miss 100% of the shots you don’t take. – Wayne Gretzky"
+    "You miss 100% of the shots you don’t take. – Wayne Gretzky",
+    "Injustice anywhere is a threat to justice everywhere. – Martin Luther King Jr.",
+    "The only thing we have to fear is fear itself. – Franklin D. Roosevelt",
+    "Imagination is more important than knowledge. – Albert Einstein",
+    "The journey of a thousand miles begins with one step. – Lao Tzu",
+    "It is never too late to be what you might have been. – George Eliot"
 ];
 ?>
