dc9f1182bce6f44dcd46f0b4a695c44418e8cfca
Tayyab Mansoor <110661825+M-Tayyab15@users.noreply.github.com>
2025-01-31 09:41:02 +0500

    Update quotes.php

--- Core Php/Random_Quotes_Generator/quotes.php
+++ Core Php/Random_Quotes_Generator/quotes.php
@@ -1,1 +1,11 @@
-
+<?php
+$quotes = [
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "The best time to plant a tree was 20 years ago. The second best time is now. – Chinese Proverb",
+    "It does not matter how slowly you go as long as you do not stop. – Confucius",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
+    "Believe you can and you're halfway there. – Theodore Roosevelt",
+    "Do not go where the path may lead, go instead where there is no path and leave a trail. – Ralph Waldo Emerson",
+    "You miss 100% of the shots you don’t take. – Wayne Gretzky"
+];
+?>
